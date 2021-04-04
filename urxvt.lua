0e90b4cd94c0c5ed1f5ab876da788057f314d61b
Landryl <Landryl@users.noreply.github.com>
2021-04-04 19:45:16 +0200

    urxvt: implementing function to send SIGHUP to all urxvt instances

--- urxvt.lua
+++ urxvt.lua
@@ -4,7 +4,7 @@
 local urxvt = {}
 
 function urxvt.refresh()
-
+    awful.spawn.with_shell("for pid in `ps -C urxvt | tail --lines=+2 | grep -Po ^\\ \\*\\[0-9\\]+`; do kill -s HUP $pid done")
 end
 
 return urxvt
