719962a182e1495fde7a57874f4dccd8982bfd3b
Landryl <Landryl@users.noreply.github.com>
2021-04-04 21:37:45 +0200

    urxvt: fixing reload function

--- urxvt.lua
+++ urxvt.lua
@@ -1,10 +1,12 @@
 -- Module to refresh URxvt configuration upong receiving a xrdb update
 -- URxvt must have config-reload extension installed
 
+local awful = require("awful")
+
 local urxvt = {}
 
-function urxvt.refresh()
-    awful.spawn.with_shell("for pid in `ps -C urxvt | tail --lines=+2 | grep -Po ^\\ \\*\\[0-9\\]+`; do kill -s HUP $pid done")
+function urxvt.reload()
+    awful.spawn.with_shell("for pid in `ps -C urxvt | tail --lines=+2 | grep -Po ^\\ \\*\\[0-9\\]+`; do kill -s HUP $pid \n done")
 end
 
 return urxvt
