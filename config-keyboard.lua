52c92ad5f7f65d58151322048830c454aa75da7b
Landryl <Landryl@users.noreply.github.com>
2021-04-13 16:11:21 +0200

    keyboard: adding bindings to open a terminal

--- config-keyboard.lua
+++ config-keyboard.lua
@@ -4,8 +4,13 @@ local awful = require("awful")
 local mod = "Mod1"
 
 globalkeys = gears.table.join(
-    awful.key({ mod, "Shift" }, "e", awesome.quit,
-              { description = "Quit Awesome", group = "Awesome"})
+    awful.key({mod, "Shift"}, "e", awesome.quit,
+              {description = "Quit Awesome", group = "Awesome"}),
+    awful.key({mod, "Shift"}, "Return",
+        function()
+            awful.spawn("urxvt", {screen = awful.screen.focused() })
+        end,
+        {description = "Open a terminal", group = "Launcher"})
 )
 
 root.keys(globalkeys)
