912b7572a5fc1657981ac7b258326f439b030072
Landryl <Landryl@users.noreply.github.com>
2021-05-28 01:06:01 +0200

    config-keyboard: configure some keys to control focus and windows

--- config-keyboard.lua
+++ config-keyboard.lua
@@ -1,16 +1,34 @@
 local gears = require("gears")
 local awful = require("awful")
 
-local mod = "Mod1"
+local mod = "Mod4"
 
 globalkeys = gears.table.join(
     awful.key({mod, "Shift"}, "e", awesome.quit,
               {description = "Quit Awesome", group = "Awesome"}),
-    awful.key({mod, "Shift"}, "Return",
+    awful.key({mod}, "Return",
         function()
             awful.spawn("urxvt", {screen = awful.screen.focused() })
         end,
-        {description = "Open a terminal", group = "Launcher"})
+        {description = "Open a terminal", group = "Launcher"}),
+
+    -- Layout management
+    awful.key({ mod }, "j", function () awful.client.focus.byidx(1) end,
+              { description = "Focus to next window by index",
+                group = "Layout" }),
+    awful.key({ mod }, "k", function () awful.client.focus.byidx(-1) end,
+              { description = "Focus to previous window by index",
+                group = "Layout" }),
+    awful.key({ mod }, "h", function () awful.tag.incmwfact(0.05) end,
+              { description = "Increase master width factor",
+                group = "Layout" }),
+    awful.key({ mod }, "l", function () awful.tag.incmwfact(-0.05) end,
+              { description = "Decrease master width factor",
+                group = "Layout" }),
+    awful.key({ mod, "Shift" }, "j", function() awful.client.swap.byidx(1) end,
+              { description = "Swap client with next by index", group = "Layout" }),
+    awful.key({ mod, "Shift" }, "k", function() awful.client.swap.byidx(-1) end,
+              { description = "Swap client with next by index", group = "Layout" })
 )
 
 root.keys(globalkeys)
