fd3498cfa9157a13bc2436e9cc4ef173368eb937
Landryl <Landryl@users.noreply.github.com>
2022-02-18 10:46:33 +0100

    pulseaudio: adding module to control pulseaudio volume

---
+++ pulseaudio.lua
@@ -0,0 +1,23 @@
+local awful = require("awful")
+
+local pa = {}
+
+function pa.get_sink_index()
+    local p = io.popen("pacmd list-sinks | grep -e 'index:' | sed 's/.*: //'", "r")
+    local str = p:read()
+    return str
+end
+
+function pa.volume_up(amount)
+    awful.spawn("pactl set-sink-volume " .. pa.get_sink_index() .. " +" .. amount .. "%")
+end
+
+function pa.volume_down(amount)
+    awful.spawn("pactl set-sink-volume " .. pa.get_sink_index() .. " -" .. amount .. "%")
+end
+
+function pa.mute()
+    awful.spawn("pactl set-sink-mute " .. pa.get_sink_index() .. " toggle")
+end
+
+return pa
